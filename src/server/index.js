f2c72a84e3e60f95aa50fa774d06cff78694d36c
oct <64427810+Octagonal-T@users.noreply.github.com>
2023-11-24 18:53:53 -0500

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,73 @@
+const fs = require("fs");
+const app = require("express")();
+const bodyParser = require('body-parser');
+var cors = require('cors');
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/login', (req, res) => {
+  let response = {status: ""};
+  let loginInfo = JSON.parse(fs.readFileSync("./db.json"));
+  if(!req.body.type){
+    if(loginInfo.students.find((e) => e.id == req.body.id).password == req.body.password){
+      response.status = "accepted";
+    }else{
+      response.status = "rejected";
+    }
+  }else{
+    if(loginInfo.students.find((e) => e.id == req.body.id).password == req.body.password){
+      response.status = "accepted";
+    }else{
+      response.status = "rejected";
+    }
+  }
+  res.send(JSON.stringify(response));
+  res.status(200);
+})
+
+app.get('/departments', (req, res) => {
+  let departmentInfo = JSON.parse(fs.readFileSync("./db.json"));
+  res.send(JSON.stringify(departmentInfo.deparments));
+  res.status(200);
+})
+
+app.post('/student/signout', (req, res) => {
+  let response = {status: ""};
+  let database = JSON.parse(fs.readFileSync("./db.json"));
+  let item = database.departments[req.body.dept.toLowerCase()].inventory.find((e) => {e.item == req.body.item}).obj.find((e) => e.trackingNum == req.body.trackingNum);
+  if(item === undefined){
+    response.status = "REJECTED";
+  }else if(item.status != "SIGNED OUT"){
+    item.student = req.body.student;
+    item.dueDate = req.body.dueDate;
+    item.dateOut = Date.now();
+    item.status = "RESERVED";
+    response.status = "ACCEPTED";
+  }else{
+    response.status = "REJECTED";
+  }
+  database.students.find((e) => {id == req.body.student}).history.push({trackingNum: req.body.trackingNum, dueDate: req.body.dueDate, dateOut: Date.now()})
+  database.departments[req.body.dept.toLowerCase()].inventory.find((e) => {e.item == req.body.item}).obj.find((e) => e.trackingNum == req.body.trackingNum) = item;
+  res.send(JSON.stringify(response));
+  res.status(200);
+  fs.writeFileSync("./db.json", JSON.stringify(database))
+})
+app.post('/student/return', (req, res) => {
+  let response = {status: ""};
+  let database = JSON.parse(fs.readFileSync("./db.json"));
+  let item = departmentInfo[req.body.dept.toLowerCase()].inventory.find((e) => {e.item == req.body.item}).obj.find((e) => e.trackingNum == req.body.trackingNum);
+  if(item === undefined){
+    response.status = "REJECTED";
+    item.status = "IN STOCK";
+    item.student = 0;
+    item.dueDate = 0;
+    item.dateOut = 0;
+    response.status = "ACCEPTED"
+  }
+  database.departments[req.body.dept.toLowerCase()].inventory.find((e) => {e.item == req.body.item}).obj.find((e) => e.trackingNum == req.body.trackingNum)
+  res.send(JSON.stringify(response));
+  res.status(200);
+})
+
+app.listen(3000);
